efdc40d890611d65e1846751bfc8107ab24ba5f2
Beezers <beezers1979@gmail.com>
2015-03-09 00:11:51 +0000

    added automated tests with Guard and amended guardfile, added spring to .gitingnore

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,5 +24,12 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@base_title}"
   end
   
+  test "should_get_contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Content | #{@base_title}"
+  end
+  
+  
 
 end
